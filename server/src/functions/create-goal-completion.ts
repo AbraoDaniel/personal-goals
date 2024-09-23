3606ac72cb70388508d94654ad2e653207d7fc80
Daniel <abraodaniel@hotmail.com>
2024-09-23 19:27:52 -0300

    creating application server and oroutes

---
+++ server/src/functions/create-goal-completion.ts
@@ -0,0 +1,61 @@
+import { count, and, gte, lte, eq, sql } from 'drizzle-orm'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import dayjs from 'dayjs'
+
+interface ICreateGoalCompletion {
+  goalId: string
+}
+
+export async function createGoalCompletion({ goalId }: ICreateGoalCompletion) {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWek = dayjs().endOf('week').toDate()
+
+  const goalCompletionCount = db.$with('goal_completion_count').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWek),
+          eq(goalCompletions.id, goalId)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const result = await db
+    .with(goalCompletionCount)
+    .select({
+      desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+      completionCount: sql`
+        COALESCE(${goalCompletionCount.completionCount}, 0)
+      `.mapWith(Number),
+    })
+    .from(goals)
+    .leftJoin(goalCompletionCount, eq(goalCompletionCount.goalId, goals.id))
+    .where(eq(goals.id, goalId))
+    .limit(1)
+
+  const { completionCount, desiredWeeklyFrequency } = result[0]
+
+  if (completionCount >= desiredWeeklyFrequency) {
+    throw new Error('Goal already completed this week!')
+  }
+
+  const insertResult = await db
+    .insert(goalCompletions)
+    .values({
+      goalId,
+    })
+    .returning()
+
+  const goalCompletion = result[0]
+  return {
+    goalCompletion,
+  }
+}
---
+++ server/src/functions/get-week-goals.ts
@@ -0,0 +1,57 @@
+import dayjs from 'dayjs'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import { and, count, gte, lte, eq, sql } from 'drizzle-orm'
+
+export async function getWeekGoals() {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWek = dayjs().endOf('week').toDate()
+
+  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWek))
+  )
+
+  const goalCompletionCount = db.$with('goal_completion_count').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWek)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const pendingGoals = await db
+    .with(goalsCreatedUpToWeek, goalCompletionCount)
+    .select({
+      id: goalsCreatedUpToWeek.id,
+      title: goalsCreatedUpToWeek.title,
+      desiredWeeklyFrequency: goalsCreatedUpToWeek.desiredWeeklyFrequency,
+      completionCount: sql`
+        COALESCE(${goalCompletionCount.completionCount}, 0)
+      `.mapWith(Number),
+    })
+    .from(goalsCreatedUpToWeek)
+    .leftJoin(
+      goalCompletionCount,
+      eq(goalCompletionCount.goalId, goalsCreatedUpToWeek.id)
+    ) // os registros de completos podem não existir, por isso left
+
+  return {
+    pendingGoals,
+  }
+}
